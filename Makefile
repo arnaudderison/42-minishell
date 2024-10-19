5af88e254da6e06149ea4440f80b57825c9c0a57
arnaudderison <arnaud.derison@hotmail.com>
2024-10-19 20:54:30 +0200

    init project

---
+++ Makefile
@@ -0,0 +1,46 @@
+NAME = minishell
+CC = gcc
+CFLAGS = -Wall -Wextra -Werror -g3
+I_LIBFT = ./include/lib/libft/include/
+I_MINISH = ./include/
+INCLUDES = -I$(I_LIBFT) -I$(I_MINISH)
+OBJ_DIR = obj
+LIBFT_DIR = ./include/lib/libft/
+LIBFT = $(LIBFT_DIR)libft.a
+
+RED=\033[0;31m
+GREEN=\033[0;32m
+YELLOW=\033[1;33m
+CYAN=\033[0;36m
+NC=\033[0m
+
+MINISH_SRCS = src/main.c $(wildcard src/*/*.c)
+MINISH_OBJS = $(patsubst src/%.c,$(OBJ_DIR)/%.o,$(MINISH_SRCS))
+
+all: $(NAME)
+
+$(NAME): $(LIBFT) $(MINISH_OBJS)
+	@$(CC) $(CFLAGS) $(INCLUDES) $(MINISH_OBJS) -L$(LIBFT_DIR) -lft -o $@
+	@echo "${YELLOW}Executable $(NAME) created.${NC}"
+
+$(LIBFT):
+	@make -C $(LIBFT_DIR)
+
+$(OBJ_DIR)/%.o: src/%.c
+	@mkdir -p $(@D)
+	@echo "${CYAN}Compiling $<...${NC}"
+	@$(CC) $(CFLAGS) $(INCLUDES) -c $< -o $@
+
+clean:
+	@make clean -C $(LIBFT_DIR)
+	@rm -rf $(OBJ_DIR)
+	@echo "${GREEN}Object files cleaned.${NC}"
+
+fclean: clean
+	@make fclean -C $(LIBFT_DIR)
+	@rm -f $(NAME)
+	@echo "${RED}All files removed.${NC}"
+
+re: fclean all
+
+.PHONY: all clean fclean re
---
+++ include/minishell.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aderison <aderison@student.s19.be>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/19 20:23:42 by aderison          #+#    #+#             */
+/*   Updated: 2024/10/19 20:30:39 by aderison         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include "enum.h"
+# include "structure.h"
+# include <stdio.h>
+# include <unistd.h>
+
+#endif
